09af244eb4eae37ef535c181b3289483e91fbdc8
Mathieu Bartozzi <mathieu.bartozzi@mlfmonde.org>
2024-11-08 14:33:11 +0100

    Ajout de l'application Streamlit pour le déploiement

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ st.set_page_config(
     layout="wide"
 )
 
+st.logo('logo.png')
+
 # # Charger les données depuis Google Sheets
 # sheet_id = "10eH5dsJ2-t_ppLPgr9TIrijOR_1DdOYEPhSSiP6BhrI"
 # sheet_name = "Feuille1"
@@ -70,11 +72,7 @@ repartition_format.columns = ['Format', 'Nombre']
 repartition_public = data_cleaned['Public'].value_counts().reset_index()
 repartition_public.columns = ['Public', 'Nombre']
 
-#calcul des metriques principales
-total_interventions = len(data_cleaned)
-total_enseignants = int(data_cleaned['Nombre d\'enseignants impactés'].sum())
-duree_totale = data_cleaned['Durée (en heure)'].sum()
-efficacite_moyenne_val = data_cleaned['Efficacité'].mean()
+
 
 #theme des graphiques
 color_grah_theme=px.colors.qualitative.T10
@@ -87,31 +85,15 @@ color_mapping_public= {
 }
 
 # Titre principal
-st.sidebar.title("CP OSUI 2024 - 2025 : tableau de bord d'activité ")
+st.title("CP OSUI 2024 - 2025 : tableau de bord d'activité ")
 
-st.write("Répartition Format:", repartition_format)
-st.write("Répartition Public:", repartition_public)
-st.write("Data_cleaned:", data_cleaned)
-
-
-# Slider de sélection de dates dans la barre latérale
-selected_date_range = st.sidebar.slider(
-    "Sélectionnez une plage de dates",
-    min_value=date_min,
-    max_value=date_max,
-    value=(date_min, date_max),
-    step=timedelta(days=1)
-)
-
-
-# Filtrer les données en fonction de la plage de dates sélectionnée
-data_cleaned = data_cleaned[
-    (data_cleaned['Date_only'] >= selected_date_range[0]) &
-    (data_cleaned['Date_only'] <= selected_date_range[1])
-]
+#calcul des metriques principales
+total_interventions = len(data_cleaned)
+total_enseignants = int(data_cleaned['Nombre d\'enseignants impactés'].sum())
+duree_totale = data_cleaned['Durée (en heure)'].sum()
+efficacite_moyenne_val = data_cleaned['Efficacité'].mean()
 
 # Debut de la page
-# st.subheader("Indicateurs généraux")
 
 #ligne 1 : indicateurs
 col1, col2, col3, col4 = st.columns(4)
@@ -132,6 +114,7 @@ with col4:
 #ligne 1 : pie chart formart + pie char format + repartation par public vs types.
 col1, col2=st.columns([1, 3])
 with col1:
+    with st.container(border=True):
         # Graphique en anneau pour la répartition des formats (sans légende pour éviter la répétition)
         fig_pie_format = px.pie(
             repartition_format,
@@ -143,22 +126,25 @@ with col1:
 
         # Positionner le titre et supprimer la légende pour ce graphique
         fig_pie_format.update_layout(
-            title_text="",
+            width=350, height=350,
             legend=dict(
             orientation="h",  # Orientation horizontale
             yanchor="bottom",
-            y=0.8,  # Positionner la légende au-dessus du graphique
+            # # Positionner la légende au-dessus du graphique
             xanchor="center",
             x=0.5  # Centrer la légende
         ),
-            margin=dict(t=20, b=0)
+            margin=dict(t=0, b=0)
         )
+
 
         # Afficher le graphique
         st.plotly_chart(fig_pie_format)
 
 
 with col2 :
+
+    with st.container(border=True):
         col1, col2 = st.columns([1, 2])
 
         with col1:
@@ -168,6 +154,7 @@ with col2 :
                 names='Public',
                 values='Nombre',
                 hole=.4,
+                color='Public',
                 color_discrete_map={
                 'degré 1': '#F58518',  # orange
                 'degré 2': '#4C78A8',  # bleu
@@ -177,15 +164,16 @@ with col2 :
 
             # Positionner le titre et supprimer la légende pour ce graphique
             fig_pie_public.update_layout(
+                width=350, height=350,
                 title_text="",
                 legend=dict(
                 orientation="h",  # Orientation horizontale
-                yanchor="bottom",
-                y=0.8,  # Positionner la légende au-dessus du graphique
+                yanchor="top",
+                y=0,  # Positionner la légende au-dessus du graphique
                 xanchor="center",
                 x=0.5  # Centrer la légende
             ),
-                margin=dict(t=20, b=0)
+                margin=dict(t=0, b=0)
             )
             # Afficher le graphique
             st.plotly_chart(fig_pie_public)
@@ -203,7 +191,10 @@ with col2 :
                 'inter degré': '#E45756'  # rouge
     }
             )
-            fig_stacked_bar_degre.update_layout(barmode='stack',showlegend=True)
+            fig_stacked_bar_degre.update_layout(
+                barmode='stack',
+                showlegend=False,
+                width=800, height=350)
             fig_stacked_bar_degre.update_xaxes(title=None)  # Supprimer le label de l'axe X
             fig_stacked_bar_degre.update_yaxes(title=None)  # Supprimer le label de l'axe Y
 
@@ -211,70 +202,71 @@ with col2 :
 
 
 # Section 2 :
-col1, col2 = st.columns(2)
+with st.container(border=True):
+    col1, col2 = st.columns(2)
+    with col1:
+        #préparations des données
+        data_expanded = data_cleaned.assign(
+        Établissement_cible=data_cleaned['Établissement cible'].str.split(',')
+        ).explode('Établissement_cible')
+        data_expanded['Établissement_cible'] = data_expanded['Établissement_cible'].str.strip()
 
-with col1:
-    #préparations des données
-    data_expanded = data_cleaned.assign(
-    Établissement_cible=data_cleaned['Établissement cible'].str.split(',')
-    ).explode('Établissement_cible')
-    data_expanded['Établissement_cible'] = data_expanded['Établissement_cible'].str.strip()
+        # Calcul du nombre total d'enseignants impactés par établissement et tri par ordre décroissant
+        impact_etablissement = (
+            data_expanded.groupby('Établissement_cible')['Nombre d\'enseignants impactés']
+            .sum()
+            .reset_index()
+            .sort_values(by='Nombre d\'enseignants impactés', ascending=False)
+        )
+        #visuel
+        fig_chart_1 = px.bar(
+            impact_etablissement,
+            x='Établissement_cible',
+            y='Nombre d\'enseignants impactés',
+            title="Nombre d'enseignants impactés par établissement",
+            color_discrete_sequence=color_grah_theme)
+        # Suppression des titres des axes
 
-    # Calcul du nombre total d'enseignants impactés par établissement et tri par ordre décroissant
-    impact_etablissement = (
-        data_expanded.groupby('Établissement_cible')['Nombre d\'enseignants impactés']
-        .sum()
-        .reset_index()
-        .sort_values(by='Nombre d\'enseignants impactés', ascending=False)
-    )
-    #visuel
-    fig_chart_1 = px.bar(
-        impact_etablissement,
-        x='Établissement_cible',
-        y='Nombre d\'enseignants impactés',
-        title="Nombre d'enseignants impactés par établissement",
-        color_discrete_sequence=color_grah_theme)
-    # Suppression des titres des axes
-    fig_chart_1.update_xaxes(title=None)
-    fig_chart_1.update_yaxes(title=None)
-    st.plotly_chart(fig_chart_1)
+        fig_chart_1.update_xaxes(title=None)
+        fig_chart_1.update_yaxes(title=None)
+        st.plotly_chart(fig_chart_1)
 
-with col2:
-    tendance = data_cleaned.groupby(pd.Grouper(key='Date')).agg({
-        'Durée (en heure)': 'sum',
-        'Nombre d\'enseignants impactés': 'sum'
-    }).reset_index()
+    with col2:
+        tendance = data_cleaned.groupby(pd.Grouper(key='Date')).agg({
+            'Durée (en heure)': 'sum',
+            'Nombre d\'enseignants impactés': 'sum'
+        }).reset_index()
 
-    # Trier par date
-    tendance = tendance.sort_values(by='Date')
+        # Trier par date
+        tendance = tendance.sort_values(by='Date')
 
-    # Calcul de la moyenne mobile sur la colonne 'Durée (en heure)' sur une fenêtre de 7 jours
-    tendance['Moyenne_mobile'] = tendance['Durée (en heure)'].rolling(window=6).mean()
+        # Calcul de la moyenne mobile sur la colonne 'Durée (en heure)' sur une fenêtre de 7 jours
+        tendance['Moyenne_mobile'] = tendance['Durée (en heure)'].rolling(window=6).mean()
 
-    # Visualisation
-    fig_chart_2 = px.bar(
-        tendance,
-        x='Date',
-        y='Durée (en heure)',
-        title="Nombre d'heures d'intervention par jour",
-        color_discrete_sequence=color_grah_theme
-    )
+        # Visualisation
+        fig_chart_2 = px.bar(
+            tendance,
+            x='Date',
+            y='Durée (en heure)',
+            title="Nombre d'heures d'intervention par jour",
+            color_discrete_sequence=color_grah_theme
+        )
 
-    # Suppression des titres des axes
-    fig_chart_2.update_xaxes(title=None, tickformat='%d/%m/%Y')
-    fig_chart_2.update_yaxes(title="Durée (en heure)")
+        # Suppression des titres des axes
+        fig_chart_2.update_xaxes(title=None, tickformat='%d/%m/%Y')
+        fig_chart_2.update_yaxes(title="Durée (en heure)")
 
-    # Ajout de la ligne de moyenne mobile
-    fig_chart_2.add_scatter(
-        x=tendance['Date'],
-        y=tendance['Moyenne_mobile'],
-        mode='lines',
-        name='Moyenne mobile (7 jours)',
-        showlegend=False,
-        fill='tozeroy',
-        line_shape='spline',
-        line=dict(color=color_grah_theme[1], width=2)
-    )
+        # Ajout de la ligne de moyenne mobile
+        fig_chart_2.add_scatter(
+            x=tendance['Date'],
+            y=tendance['Moyenne_mobile'],
+            mode='lines',
+            name='Moyenne mobile (7 jours)',
+            showlegend=False,
+            fill='tozeroy',
+            line_shape='spline',
+            line=dict(color=color_grah_theme[1], width=2)
+        )
 
-    # Affichage du graphique dans Streamlit
-    st.plotly_chart(fig_chart_2)
+        # Affichage du graphique dans Streamlit
+        st.plotly_chart(fig_chart_2)
